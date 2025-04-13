0398048200ed562bae3332e99c56263d76691089
Blooym <19539165+Blooym@users.noreply.github.com>
2025-04-13 23:24:01 +0100

    feat: show zenity prompts for failures if supported

--- steam-tools/GE-Proton/launch.sh
+++ steam-tools/GE-Proton/launch.sh
@@ -9,17 +9,13 @@ proton_entrypoint=$tooldir/proton/proton
 echo "-- Checking for updates --"
 if ! "$tooldir/use-proton-latest" --install-dir-base="$tooldir" --repo-owner="GloriousEggroll" --repo-name="proton-ge-custom" --tarball-name="GE-Proton"; then
     if [ -f "$proton_entrypoint" ]; then
+    	zenity --info --title "Update Failed" --text "Something went wrong when updating Proton-GE to a new version, an older version will be used instead." --no-wrap --no-markup || true
         echo "Warning: use-proton-latest exited unsuccessfully, continuing with old proton install."
     else
+    	zenity --error --title "Install Failed" --text "Something went wrong when installing Proton-GE, aborting launch." --no-wrap --no-markup || true
         echo "FATAL: use-proton-latest failed and $proton_entrypoint does not exist."
         exit 1
     fi
-fi
-
-echo "-- Starting proton --"
-if [ ! -f "$tooldir/proton/proton" ]; then
-    echo "FATAL: $tooldir/proton/proton not found."
-    exit 1
 fi
 
 "$proton_entrypoint" "$@"
